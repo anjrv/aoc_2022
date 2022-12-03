4280dcfe2423bb0beb9b7f39b952eda9a9e0b163
Anna Jaerving <AnnaJrving@gmail.com>
2022-12-03 16:07:10 +0000

    first 3

--- rust_solutions/src/bin/solution_3.rs
+++ rust_solutions/src/bin/solution_3.rs
@@ -6,7 +6,14 @@ fn main() {
     let mut priority = 0;
 
     for lines in &stdin().lock().lines().chunks(3) {
-        let shared = lines.reduce(|a, b| Ok(a.unwrap().chars().collect::<Vec<char>>().intersect(b.unwrap().chars().collect()).iter().collect::<String>()));
+        let shared = lines.reduce(|a, b| {
+            Ok(a.unwrap()
+                .chars()
+                .collect::<Vec<char>>()
+                .intersect(b.unwrap().chars().collect())
+                .into_iter()
+                .collect::<String>())
+        });
 
         if let Ok(val) = shared.unwrap() {
             let p = val.chars().take(1).last().unwrap() as u32;
