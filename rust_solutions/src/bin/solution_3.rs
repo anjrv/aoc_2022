7e93dfd6c380f5c5e2db68f2f53b53a8677d503f
Anna Jaerving <AnnaJrving@gmail.com>
2022-12-04 14:18:55 +0000

    4

--- rust_solutions/src/bin/solution_3.rs
+++ rust_solutions/src/bin/solution_3.rs
@@ -6,20 +6,22 @@ fn main() {
     let mut priority = 0;
 
     for lines in &stdin().lock().lines().chunks(3) {
-        let shared = lines.reduce(|a, b| {
-            Ok(a.unwrap()
-                .chars()
-                .collect::<Vec<char>>()
-                .intersect(b.unwrap().chars().collect())
-                .into_iter()
-                .collect::<String>())
-        });
+        let shared = lines
+            .reduce(|a, b| {
+                Ok(a.unwrap()
+                    .chars()
+                    .collect::<Vec<char>>()
+                    .intersect(b.unwrap().chars().collect())
+                    .into_iter()
+                    .collect::<String>())
+            })
+            .unwrap();
 
-        if let Ok(val) = shared.unwrap() {
+        if let Ok(val) = shared {
             let p = val.chars().take(1).last().unwrap() as u32;
             priority += if p > 90 { p - 96 } else { p - 38 };
         }
     }
 
-    println!("{:?}", priority);
+    println!("{priority}");
 }
